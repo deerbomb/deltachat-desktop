2b26b2e0e37661c38d5b2bee944890d1feafd296
jikstra <jikstra@disroot.org>
2020-10-05 11:57:53 +0200

    Get rid of ipcBackend event emit to trigger updateTrayIcon, instead catch it on handleDesktopSettings in backend

--- src/renderer/components/dialogs/Settings.tsx
+++ src/renderer/components/dialogs/Settings.tsx
@@ -29,7 +29,6 @@ import {
 import SettingsBackup from './Settings-Backup'
 import SettingsAccount from './Settings-Account'
 import SettingsAppearance from './Settings-Appearance'
-import { ipcBackend } from '../../ipc'
 
 function flipDeltaBoolean(value: string) {
   return value === '1' ? '0' : '1'
@@ -116,9 +115,6 @@ export default function Settings(props: DialogProps) {
       (await DeltaBackend.call('settings.setDesktopSetting', key, value)) ===
       true
     ) {
-      if (key === 'minimizeToTray') {
-        ipcBackend.send('updateTrayIcon')
-      }
       setDesktopSetting(key, value)
     }
   }
